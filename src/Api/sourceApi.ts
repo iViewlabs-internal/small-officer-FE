f7ff06f9cff7c286775965e5f3f08613e0df8a1e
Bhavisha-iview <bhavisha.nayi@iviewlabs.net>
2022-10-31 16:06:14 +0530

    changed api

--- src/Api/sourceApi.ts
+++ src/Api/sourceApi.ts
@@ -1,3 +1,5 @@
 // export const api = 'https://small-officer.herokuapp.com/api/user';
 
-export const api = 'http://localhost:8000/api/user';
+// export const api = 'http://localhost:8000/api/user';
+
+export const api = 'https://small-officer-backend.herokuapp.com/api/user'
